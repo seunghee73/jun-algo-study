4531a8cf0ac8438db22deb2db4676be124bf3987
김광용 <veta6287@naver.com>
2022-06-03 18:25:25 +0900

    0603 yong

--- 0603/1041_yong.py
+++ 0603/1041_yong.py
@@ -1,16 +1,21 @@
 # 각 면에 따라 정해진 규칙이 존재
 # 마주보는 면의 최소값을 기반으로 풀 수 있었던 문제
+# -------------------------------------
+# N이 1인경우를 고려안해수 수정
 
 N = int(input())
-cnt = N * N * 5 - 12 - (N-1)*(N-2) * 4 - (N-2) ** 2
 ans = 0
 lst = list(map(int, input().split()))
-v = []
-v.append(min(lst[0], lst[5]))
-v.append(min(lst[1], lst[4]))
-v.append(min(lst[2], lst[3]))
-v.sort()
-ans = sum(v) * 4
-ans += ((N-1)*(N-2) * 4 * min(lst) + (N-2) ** 2 * min(lst))
-ans += (sum(v[:2]) * cnt//2)
+if N == 1:
+    lst.sort()
+    ans = sum(lst[:5])
+else:
+    v = []
+    v.append(min(lst[0], lst[5]))
+    v.append(min(lst[1], lst[4]))
+    v.append(min(lst[2], lst[3]))
+    v.sort()
+    ans = sum(v) * 4
+    ans += ((N-1)*(N-2) * 4 + (N-2) ** 2) * v[0]
+    ans += sum(v[:2]) * (4 * (N-1) + 4 * (N-2)) 
 print(ans)
